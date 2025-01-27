0590994c0c80ad5d008edfd8673a22e841879741
Stephanie Rinaldi <srinaldi@arizona.edu>
2025-01-27 09:48:23 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+# Testing / not finished.
+
+# Argument Settings -------------------------------------------------
+## Script Directory / File to run
+SCRIPT="scripts/run_report.py"
+
+## Budget yaml data file names
+## ADJUST these file names to be what the budget yaml files actually are within the data directory that are to be used for the reports.
+ML="mission_lifetime.yaml"
+WFE="wavefront_error.yaml"
+TR="transient_response.yaml"
+
+# Installing package (if not done already or modified)
+pip install .
+
+# Running Scripts  --------------------------------------------------
+## Adjust this if you want only a specific file.
+## Can you command-line input instead / this is more for testing
+python "${SCRIPT}" "${ML}" "${WFE}" "${TR}"
