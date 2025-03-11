a13406784649af6476d9974ad05d8155a225c8b3
Stephanie Rinaldi <srinaldi@arizona.edu>
2025-03-11 13:27:00 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,10 +5,12 @@
 SCRIPT="scripts/run_report.py"
 
 ## Budget yaml data file names
-## ADJUST these file names to be what the budget yaml files actually are within the data directory that are to be used for the reports.
+## ADJUST these file names to be what the budget yaml files actually are within the `data` directory that are to be used for the reports.
 ML="mission_lifetime.yaml"
 WFE="wavefront_error.yaml"
 TR="transient_response.yaml"
+TEST="test_budget.yaml"
+PL="test_plantuml.yaml"
 
 # Installing package (if not done already or modified)
 pip install .
@@ -17,3 +19,6 @@ pip install .
 ## Adjust this if you want only a specific file.
 ## Can you command-line input instead / this is more for testing
 python "${SCRIPT}" "${ML}" "${WFE}" "${TR}"
+
+# For using the gui interface instead for diagrams w/plantuml
+#java -jar plantuml.jar -gui
