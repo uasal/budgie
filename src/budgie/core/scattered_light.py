4f9ecac11e4fab0a38f886ea3991d95c9a76e4bc
Patrick Ingraham <pingraham@arizona.edu>
2025-05-05 23:12:30 -0700

    Update to work with new pst file format (v2). End of night push

--- src/budgie/core/scattered_light.py
+++ src/budgie/core/scattered_light.py
@@ -14,6 +14,7 @@ import pandas as pd
 import sys
 import utils_config
 import pytest
+from astropy import units as u
 
 
 # Is there a more pythonic way to do this?
@@ -73,22 +74,31 @@ class ScatteredLight(Budget):
         # FIXME - get Lazuli FoV!!!
         # Using values from Pearl white paper
         logger.warning("FOV is hardcoded!")
-        self.x_fov = 0.115 * 2  # [degrees]
-        self.y_fov = 0.043 * 2  # [degrees]
+        # From Max: FoV size - +/- 0.27 degrees in X, and -0.15 to 0.075
+        self.x_center = 0.0 * u.Unit('deg')
+        self.y_center = (-0.15+0.075)/2 * u.Unit('deg')
+        self.x_fov = 0.27*2 * u.Unit('deg')  # [degrees]
+        self.y_fov = (0.075+0.150) * u.Unit('deg')  # [degrees]
 
         # vega V flux from https://www.gemini.edu/observing/resources/magnitudes-and-fluxes (3.68E-08 W/m2/um)
         # Multiply by 540nm (Johnson V-band central wavelength https://en.wikipedia.org/wiki/Photometric_system)
+        _vega_flux = 3.68e-08 * u.Unit('W/(m2 um)')
+        self.wave_cen = 540e-9 * u.Unit('m') # central wavelength
         self.vega_flux = {
-            "V": 3.68e-08 * (540e-9 * 1e6)
+            "V": (_vega_flux * self.wave_cen).to('W / m2')
         }  # Irradiance of Vega (in W/m^2)
-        logger.debug(f"{self.vega_flux["V"]=:0.3e} [W/m2]")
+        logger.debug(f"{self.vega_flux["V"]=:0.3e}")
         self.vega_mag = {"V": 0}  # setting incase of zero point offsets.
+
+        # Get pupil area
+        # note the 0.5's because the diameters are in the configs and not radii
+        self.pupil_area = np.pi * ((0.5*u.Quantity(config['telescope']['optics']['m1']['aper_clear_OD']))**2) - np.pi*((0.5*u.Quantity(config['telescope']['optics']['m1']['aper_clear_ID']))**2)
 
     def plot_pst(self, theta, pst, title, color, label, xlim=None, ylim=None):
         # determine range
         if xlim is None:
-            xmax = np.nanmax(theta[pst > 1e-8])
-            xmin = np.nanmin(theta[pst > 1e-8])
+            xmax = np.nanmax(theta[pst.value > 1e-8].value)
+            xmin = np.nanmin(theta[pst.value > 1e-8].value)
             xlim = (xmin, xmax)
         if ylim is None:
             ymax = 2
@@ -96,7 +106,7 @@ class ScatteredLight(Budget):
             ylim = (ymin, ymax)
 
         plt.figure(figsize=(8, 5))
-        plt.plot(theta, pst, marker="o", linestyle="-", color=color, label=label)
+        plt.plot(theta.value, pst.value, marker="o", linestyle="-", color=color, label=label)
         plt.xlim(xlim)
         plt.ylim(ylim)
         plt.yscale("log")
@@ -107,23 +117,13 @@ class ScatteredLight(Budget):
         plt.grid(True, which="both", linestyle="--")
         plt.show(block=False)
 
-    def calc_plate_scale(self):
-        """Calculates the plate scale"""
-
-    def calc_throughput(self):
-        """Calculates throughput of the telescope."""
-
-        logger.warning("Throughput is a placeholder and needs updating")
-        throughput = 0.95**4
-
-        return throughput
 
     def run_report(self, output_dir):
         # Method that is called by a generic script
         # needs to be in every budget class.
 
         # Read in PST data (theta and power in y and x dims)
-        # Output needs to be normalized to center of the FoV, units in W/m2
+        # Output is in W/m2 at the focal plane
 
         # Note that the filename is relative to the budget location
         pst_file = Path(self.scattered.budget_dir).joinpath(
@@ -133,52 +133,18 @@ class ScatteredLight(Budget):
         file_type = self.scattered.budget["inputs"]["pst_file"]["file_type"]
         theta_y, pst_y, theta_x, pst_x = self.read_pst(pst_file, file_type=file_type)
 
-        pst_no_scatter_file = Path(self.scattered.budget_dir).joinpath(
-            self.scattered.budget["inputs"]["pst_no_scatter_file"]["value"]
-        )
-        file_type = self.scattered.budget["inputs"]["pst_no_scatter_file"]["file_type"]
-
-        theta_y_ns, pst_y_ns, theta_x_ns, pst_x_ns = self.read_pst(
-            pst_no_scatter_file, file_type=file_type
-        )
-
-        # verify that the angles are the same
-        # to within an arcsecond
-        for i, theta in enumerate(theta_y):
-            assert theta == pytest.approx(theta_y_ns[i], abs=1 / 3600.0)
-        for i, theta in enumerate(theta_x):
-            assert theta == pytest.approx(theta_x_ns[i], abs=1 / 3600.0)
-
-        # Normalize PSTs
-        if file_type == "fred_xlsx_v1":
-            # psts are output as dataframes
-            theta_y_full, pst_y_full_norm = self.pst_normalize_fred(
-                theta_y, pst_y, pst_y_ns, axis="y"
-            )
-            theta_x_full, pst_x_full_norm = self.pst_normalize_fred(
-                theta_x, pst_x, pst_x_ns, axis="x"
-            )
-
-        # # Plot raw PST Y data
-        # plot_pst(theta_y_full, pst_y_full, r"PST for $\theta_y$ (Full Field)", 'b', "PST Y Full Range")
-
-        # Plot normalized PST Y data
-        # self.plot_pst(theta_y_full, pst_y_full_norm, r"Normalized PST for $\theta_y$ (Full Field)", 'r', "Normalized PST Y Full Range")
-
-        # # Plot raw PST X data
-        # plot_pst(theta_x_full, pst_x_full, r"PST for $\theta_x$ (Full Field)", 'b', "PST X Full Range")
 
         # # Plot normalized PST X data
         self.plot_pst(
-            theta_x_full,
-            pst_x_full_norm,
+            theta_x,
+            pst_x,
             r"Normalized PST for $\theta_x$ (Full Field)",
             "r",
             "Normalized PST X Full Range",
         )
         self.plot_pst(
-            theta_y_full,
-            pst_y_full_norm,
+            theta_y,
+            pst_y,
             r"Normalized PST for $\theta_y$ (Full Field)",
             "r",
             "Normalized PST Y Full Range",
@@ -187,16 +153,16 @@ class ScatteredLight(Budget):
 
         # Declare PSTs for the class
 
-        self.theta_x = np.array(theta_x_full, dtype=float)
-        self.pst_x = np.array(pst_x_full_norm, dtype=float)
+        self.theta_x = np.array(theta_x, dtype=float)
+        self.pst_x = np.array(pst_x, dtype=float)
 
-        self.theta_y = np.array(theta_y_full, dtype=float)
-        self.pst_y = np.array(pst_y_full_norm, dtype=float)
+        self.theta_y = np.array(theta_y, dtype=float)
+        self.pst_y = np.array(pst_y, dtype=float)
 
-        #################################
-        # Estimate sky coverage based on the brightness of a single star
-        #  and it's relative location
-        #################################
+        ###################################################
+        # Estimate sky coverage based on the brightness of 
+        # a single star and it's relative location
+        ###################################################
 
         # Relates a stellar magnitude and a PST value.
         # So what PST value corresponds to
@@ -228,22 +194,16 @@ class ScatteredLight(Budget):
         # estimate sky coverage by looking at the brightest stars
         logger.debug(f"Starting sky coverage calculation")
 
-        # Convert to Watts/m^2/arcsec for a 0th magnitude star
-
-        # Scale to magnitude Xth incident star
-
-        # Calculate surface flux in zodi's and compare to requirement
-        # Note requirement should be something like X% coverage above a surface magnitude of X zodi's.
-
-        # calculate in a surface magnitude
-
         # Determine coverage for an Xth magnitude star with a radius of R
 
     def read_pst(self, filename, file_type=None):
         """Reads PST files. Currently only xlsx is supported which is the file
         that is exported by the FRED model.
 
-        Output needs to be normalized to center of the FoV, units in W/m2
+        Values is normalized by the power incident on the entrance aperture so that
+        on-axis (and in field), the PST value will effectively be the throughput.
+
+        Output is therefore unitless.
 
         Parameters:
         -----------
@@ -257,25 +217,33 @@ class ScatteredLight(Budget):
         Returns:
         --------
         pst_y: array
-            PST of y-axis [W/m2] but normalized by the value at the center of the field.
+            PST of y-axis [W/m2].
         pst_x: array
-            PST of x-axis [W/m2] but normalized by the value at the center of the field.
+            PST of x-axis [W/m2].
 
         """
 
         # determine input file type
-        if file_type == "fred_xlsx_v1":
-            theta_y, pst_y = self.read_pst_fred(filename, axis="y")
-            theta_x, pst_x = self.read_pst_fred(filename, axis="x")
+        if "fred_xlsx_v1" in file_type:
+            raise OSError("fred_xlsx_v1 is no longer supported.")
+        
+        elif "fred_xlsx_v2" in file_type:
+            theta_y, pst_y = self.read_pst_fred(filename, axis="y", file_type=file_type)
+            theta_x, pst_x = self.read_pst_fred(filename, axis="x", file_type=file_type)
         else:
             raise ValueError(f"Only xlsx file extensions supported.")
 
         return theta_y, pst_y, theta_x, pst_x
 
-    def read_pst_fred(self, filename, axis=None):
+    def read_pst_fred(self, filename, axis=None, file_type=None):
         """Reads a PST file generated by FRED in excel (xlsx) format and extract into a pandas dataframe.
         Needs to be called for each axis.
-        Output normalized to center of the FoV, units in W/m2
+
+        Note that the power in the spreadsheet is in W/m2 as measured at the focal plane.
+        This is normalized by the power incident on the entrance aperture so that
+        on-axis (and in field), the PST value will effectively be the throughput.
+
+        Output is therefore unitless.
 
         Parameters:
         -----------
@@ -295,23 +263,16 @@ class ScatteredLight(Budget):
 
         """
 
-        def _extract_data(df, start, end, col):
-            """Extract data function.
-            FIXME: I don't really understand what this does yet.
-            """
-
-            data = df.iloc[start:end, col].reset_index(
-                drop=True
-            )  # Reset index for alignment
-            return data
+        if file_type == None:
+            raise OSError("A filetype must be provided. Supported filetypes are fred_xlsx_v2")
 
         if "xlsx" in str(filename):
             if axis == "y":
                 pst_df = pd.read_excel(
-                    filename, sheet_name=0
+                    filename, sheet_name="y_pst",header=1
                 )  # First sheet (Y-axis PST)
             elif axis == "x":
-                pst_df = pd.read_excel(filename, sheet_name="x_pst")  # X-axis PST
+                pst_df = pd.read_excel(filename, sheet_name="x_pst", header=1)  # X-axis PST
             else:
                 raise ValueError(
                     f"Axis parameter must be set to 'x' or 'y' and not {axis}."
@@ -319,61 +280,37 @@ class ScatteredLight(Budget):
         else:
             raise ValueError(f"Only xlsx file extensions supported.")
 
-        if axis == "x":
-            row_min = 2
-            row_max = 237
-        elif axis == "y":
-            row_min = 2
-            row_max = 225
+        # Revert to numpy arrays:
+        if file_type == 'fred_xlsx_v2':
+            theta = pst_df['Angle'].to_numpy(dtype=float) *u.Unit('deg')
+            pst = pst_df['Power at Detector aka PST (P_d)'].to_numpy(dtype=float) *u.Unit('W / m2')
+            incident_power = pst_df['Power at First Vane (P_1)'].to_numpy(dtype=float)*u.Unit('W / m2')
+            incident_area = pst_df['Projected Area Size of First Vane (A_1)'].to_numpy(dtype=float) *u.Unit('mm2')
+        else:
+            raise OSError('Only file_type of fred_xlsx_v2 is supported.')
 
-        theta_full = _extract_data(pst_df, row_min, row_max, 0)
-        pst_full = _extract_data(pst_df, row_min, row_max, 3)
+        
+        # Area of entrance for simulation is slightly different than
+        # the area of the entrance pupil, so need to scale slightly.
+        if axis == "y":
+            area = np.interp(self.y_center, theta, incident_area)
+            power = np.interp(self.y_center, theta, incident_power)
+        elif axis == "x":
+            area = np.interp(self.x_center, theta, incident_area)
+            power = np.interp(self.x_center, theta, incident_power)
+        else:
+            raise OSError('No axis specified.')
+        
+        scaling_factor = self.pupil_area.to('mm2') / area
+        logger.debug(f'Area scaling factor is {scaling_factor}')
+        pst = pst * scaling_factor
 
-        return theta_full, pst_full
+        # Now normalize by the light incident on the entrance pupil
+        pst/=power
 
-    def pst_normalize_fred(self, angles, pst, pst_no_scatter, axis=None):
-        """Normalizes Fred PSTs"""
+        return theta, pst
 
-        def _safe_normalize(pst, pst_ns):
-            """Function for safe normalization that fully avoids division by zero.
-            FIXME: Don't understand why this is doing this like this"""
-
-            pst = pst.to_numpy()
-            pst_ns = pst_ns.to_numpy()
-
-            # Ensure both arrays have the same length
-            min_length = min(len(pst), len(pst_ns))
-            pst = pst[:min_length]
-            pst_ns = pst_ns[:min_length]
-
-            # Initialize with original values instead of zeros
-            pst_norm = pst.copy()
-
-            # Where ns values are nonzero, apply normalization
-            nonzero_mask = pst_ns != 0
-            pst_norm[nonzero_mask] = pst[nonzero_mask] / pst_ns[nonzero_mask]
-
-            return pd.Series(pst_norm)  # Convert back to Pandas Series for plotting
-
-        def _match_lengths(theta, pst):
-            """Don't understand why this is required either"""
-            min_length = min(len(theta), len(pst))
-            return theta[:min_length], pst[:min_length]
-
-        pst_full_norm = _safe_normalize(pst, pst_no_scatter)
-        theta_norm, pst_norm = _match_lengths(angles, pst_full_norm)
-
-        assert len(theta_norm) == len(pst_norm)
-
-        # # Normalize safely (if ns = 0, use original)
-        # pst_y_full_norm = self.safe_normalize(pst_y_full, pst_y_full_ns)
-        # pst_x_full_norm = self.safe_normalize(pst_x_full, pst_x_full_ns)
-
-        # theta_x_full, pst_x_full_norm = self.match_lengths(theta_x_full, pst_x_full_norm)
-        # theta_y_full, pst_y_full_norm = self.match_lengths(theta_y_full, pst_y_full_norm)
-
-        return theta_norm, pst_norm
-
+    
     def zodi_to_surf_flux(self, zodis, wave=540e-9):
         """Return surface magnitudes for an array of zodical light levels.
         Note that 1 zodi is 22.1 mag/arcsec^2.
@@ -399,14 +336,14 @@ class ScatteredLight(Budget):
         # From the hubble paper
         # 5.17E-18 ergcm−2 s−1 Å−1 arcsec−2
         # 10^7 ergs/ Joule
-        zodi_flux1 = 5.17e-18 / 1e7 * (100**2) * 5500  # W/m2/arcsec2 at 0.55um
+        zodi_flux1 = (5.17e-18 / 1e7 * (100**2) * 5500)*u.Unit('W / (m2 arcsec2)')  # W/m2/arcsec2 at 0.55um
         logger.debug(f"{zodi_flux1=}")
 
         # From Levasseur
         zodi_flux2 = 195 * 1.261e-8 * 0.55 / 4.2545e10  # W/m2/arcsec2
         logger.debug(f"{zodi_flux2=}")
 
-        # Use the hubble paper
+        # Use the Hubble paper
         self.zodi_flux = zodi_flux1
 
         # What is the surface flux at each zodi?
@@ -419,7 +356,8 @@ class ScatteredLight(Budget):
         return surface_fluxes, surface_mags
 
     def calc_surface_flux(self, stellar_mags, separations):
-        """Determine total surface flux and magnitude for an array of stars
+        """Determine total surface flux and magnitude for an array of stars.
+
 
         Parameters:
         -----------
@@ -462,8 +400,8 @@ class ScatteredLight(Budget):
                 10 ** (-(mag - self.vega_mag["V"]) / 2.5) * self.vega_flux["V"]
             )  # W/m2
             logger.debug(f"{star_flux_at_M1=:0.3e} [W/m2]\n")
-            # interpolate PST to input angle
 
+            # interpolate PST to input angle
             star_pst_x = np.interp(star_theta_x, self.theta_x, self.pst_x)
             star_pst_y = np.interp(star_theta_y, self.theta_y, self.pst_y)
 
@@ -478,7 +416,7 @@ class ScatteredLight(Budget):
 
             star_flux_per_WCC = star_flux_at_M1 * star_pst[i]  # W/m2/FoV
 
-            fov_arcsec = (self.x_fov * self.y_fov) * 3600**2  # degrees to arcseconds
+            fov_arcsec = (self.x_fov * self.y_fov).to('arcsec2') # degrees to arcseconds
             star_flux_at_WCC = star_flux_per_WCC / (fov_arcsec)  # W/m2/arcsec
             logger.debug(f"{i=},{mag=:0.2f},{star_flux_at_WCC=:0.3e}\n")
 
